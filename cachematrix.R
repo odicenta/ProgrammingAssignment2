43bb58970ebea49a201228bd88f68df49806c797
Oscar D <odicenta@gmail.com>
2015-03-16 21:21:28 +0100

    First commit of the exercise, just adding the comments and checking that all works as intended.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates the inverse of a matrix. In order to save time and resources, 
+## first check if the inverse already exists from previous operations, if so
+## it loads it from the cache, if not, it calculates it and stores it in 
+## cache for later use.
 
-## Write a short comment describing this function
+## This helper function creates a special matrix that:
+## 1. Set the matrix
+## 2. Get the matrix
+## 3. Set the inverse of the matrix
+## 4. Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix using the makeCacheMatrix function
+## if the matrix already exists, it retrieves it from the cache, if not, it calculates 
+## the inverse and store it for later use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
