2a28646015c8f7b62d1c02b0b419900e9224743c
Oscar D <odicenta@gmail.com>
2015-03-16 21:41:15 +0100

    First check. It tries to repeat a similar process but it does not work yet as $ is invalid for atomic vectors so we need to address this

--- cachematrix.R
+++ cachematrix.R
@@ -10,14 +10,43 @@
 ## 4. Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## The get/set method are similar to provided example, using matrix type
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    
+    ## this is where we calculate the inverse
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    
+    ## finally we provide the list of available funct.
+    list(set = set, get = get,
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## This function calculates the inverse of a matrix using the makeCacheMatrix function
-## if the matrix already exists, it retrieves it from the cache, if not, it calculates 
-## the inverse and store it for later use.
+## This function calculates the inverse of a matrix using the makeCacheMatrix 
+## function if the matrix already exists, it retrieves it from the cache, if 
+## not, it calculates the inverse and store it for later use.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    ## check if it is already in the cache and return it if so
+    if(!is.null(m)){
+        message("getting the cached inverse matrix")
+        return(m)
+    }
+    
+    ## if it is not in cache, it sends the matrix to cache calculating the
+    ## inverse in the process and storing it
+    data <- x$get
+    m <- global(data, ...)
+    x$setinverse(m)
+    ## finally it returns the inverse matrix
+    m
 }
